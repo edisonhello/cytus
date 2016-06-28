f2adf60c019d70c299c682c37a9b616bf4fcba0d
edisonhello <edisonhello@hotmail.com>
2016-06-28 22:18:47 +0800

    chap m done

--- modules/getSongInfo.js
+++ modules/getSongInfo.js
@@ -107,25 +107,49 @@ module.exports = function(original){
         case "chapter_l10":
             return getReturn("L", 10, "L10 : IN MEMORY OF MANEO.", 9, 9);
         case "chapter_l01_d":
-            return getReturn("L", 1, "L1 : THE DEVASTATED", 8, 9);
+            return getReturn("Ln", 1, "L1 : THE DEVASTATED", 8, 9);
         case "chapter_l02_d":
-            return getReturn("L", 2, "L2 : ASCENSION TO THE UPPER", 7, 9);
+            return getReturn("Ln", 2, "L2 : ASCENSION TO THE UPPER", 7, 9);
         case "chapter_l03_d":
-            return getReturn("L", 3, "L3 : ROAR, ON THE DESERTED LANDS", 7, 9);
+            return getReturn("Ln", 3, "L3 : ROAR, ON THE DESERTED LANDS", 7, 9);
         case "chapter_l04_d":
-            return getReturn("L", 4, "L4 : THE UNREVEALED", 7, 9);
+            return getReturn("Ln", 4, "L4 : THE UNREVEALED", 7, 9);
         case "chapter_l05_d":
-            return getReturn("L", 5, "L5 : THE REVEALED", 7, 8);
+            return getReturn("Ln", 5, "L5 : THE REVEALED", 7, 8);
         case "chapter_l06_d":
-            return getReturn("L", 6, "L6 : VIZ AWAKENED", 8, 9);
+            return getReturn("Ln", 6, "L6 : VIZ AWAKENED", 8, 9);
         case "chapter_l07_d":
-            return getReturn("L", 7, "L7 : ALMIGHTY", 8, 9);
+            return getReturn("Ln", 7, "L7 : ALMIGHTY", 8, 9);
         case "chapter_l08_d":
-            return getReturn("L", 8, "L8 : METEOR – FAREWELL", 8, 9);
+            return getReturn("Ln", 8, "L8 : METEOR – FAREWELL", 8, 9);
         case "chapter_l09_d":
-            return getReturn("L", 9, "L9 : METRO - MOURN", 6, 9);
+            return getReturn("Ln", 9, "L9 : METRO - MOURN", 6, 9);
         case "chapter_l10_d":
-            return getReturn("L", 10, "L10 : IN MEMORY OF MANEO.", 6, 8);
+            return getReturn("Ln", 10, "L10 : IN MEMORY OF MANEO.", 6, 8);
+        case "chapter_m01":
+            return getReturn("M", 1, "Stardust Sphere", 6, 9);
+        case "chapter_m02":
+            return getReturn("M", 2, "The Ricochet", 7, 9);
+        case "chapter_m02_2":
+            return getReturn("M", "2_", "The Long Years", 5, 9);
+        case "chapter_m03":
+            return getReturn("M", 3, "The Sacred Story", 4, 9);
+        case "chapter_m04":
+            return getReturn("M", 4, "AREA 184 -Platinum mix-", 5, 9);
+        case "chapter_m05":
+            return getReturn("M", 5, "Gardenia", 3, 9);
+        case "chapter_m06":
+            return getReturn("M", 6, "Sweetness Overload!!!", 5, 9);
+        case "chapter_m07":
+            return getReturn("M", 7, "Les Parfums de Celebrez", 3, 9);
+        case "chapter_m08":
+            return getReturn("M", 8, "Afterglow", 4, 9);
+        case "chapter_m09":
+            return getReturn("M", 9, "GENESYS", 7, 9);
+        case "chapter_m10":
+            return getReturn("M", 10, "Storia", 7, 9);
+        case "chapter_m_theme":
+            return getReturn("M", 0, "The Beginning", 3, 7);
         default:
             return original;
     }
